781b12519facf86ed572f0373205a773c7503b2a
DAN090909 <alamarioda1@gmail.com>
2024-09-27 02:17:24 +0800

    test commit

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//boang
-// Redirect to the appropriate dashboard based on user rolesdasdsa
+baho ka
+// Redirect to the appropriate dashboard based on user role
 Route::get('/dashboard', function () {
     $user = auth()->user();
 
